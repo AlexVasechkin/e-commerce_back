4f8aebead96101e6e768c76b29bf245244218ff0
Alex Vasechkin <vasechkin.alex.v@gmail.com>
2022-11-17 01:04:10 +0300

    Методы для работы с категориями

--- src/Controller/Api/V1/ProductCategoryController.php
+++ src/Controller/Api/V1/ProductCategoryController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductCategoryController extends AbstractController
 {
     /**
-     * @Route("/api/v1/private/product-category/{$id}")
+     * @Route("/api/v1/private/product-category/{$id}", methods={"GET"})
      */
     public function getInstance($id, ProductCategoryRepository $productCategoryRepository)
     {
@@ -45,7 +45,89 @@ class ProductCategoryController extends AbstractController
         ProductCategoryRepository $productCategoryRepository
     ) {
         $requestParams = $httpRequest->toArray();
+
+        $pc = (new ProductCategory())
+            ->setCode($requestParams['code'] ?? null)
+            ->setName($requestParams['name'] ?? null)
+            ->setIsActive(true)
+        ;
+
+        if (isset($requestParams['parentId'])) {
+            $parentCategory = $productCategoryRepository->findOneBy(['id' => $requestParams['parentId']]);
+            if ($parentCategory) {
+                $pc->setParent($parentCategory);
+            }
+        }
+
+        $productCategoryRepository->save($pc);
+
+        return $this->json([
+            'payload' => $this->serialize($pc)
+        ]);
     }
 
+    /**
+     * @Route("/api/v1/private/product-category/list", methods={"GET"})
+     */
+    public function getList(
+        ProductCategoryRepository $productCategoryRepository
+    ) {
+        return $this->json([
+            'payload' => array_map(function (ProductCategory $pc) {
+                return $this->serialize($pc);
+            }, $productCategoryRepository->findAll())
+        ]);
+    }
 
+    private function serializeForDict(ProductCategory $entity): array
+    {
+        return [
+            'value' => $entity->getId(),
+            'caption' => $entity->getName()
+        ];
+    }
+
+    /**
+     * @Route("/api/v1/private/product-category/dict", methods={"GET"})
+     */
+    public function getDict(
+        ProductCategoryRepository $productCategoryRepository
+    ) {
+        return $this->json([
+            'payload' => array_map(function (ProductCategory $pc) {
+                return $this->serializeForDict($pc);
+            }, $productCategoryRepository->findAll())
+        ]);
+    }
+
+    /**
+     * @Route("/api/v1/private/product-category/update", methods={"POST"})
+     */
+    public function update(
+        Request $httpRequest,
+        ProductCategoryRepository $productCategoryRepository
+    ) {
+        $rp = $httpRequest->toArray();
+
+        $id = $rp['id'] ?? null;
+
+        $pc = $productCategoryRepository->findOneBy(['id' => $id]);
+
+        isset($rp['code']) ? $pc->setCode($rp['code']) : null;
+
+        isset($rp['name']) ? $pc->setName($rp['name']) : null;
+
+        isset($rp['isActive']) ? $pc->setIsActive($rp['isActive']) : null;
+
+        if (isset($rp['parentId'])) {
+            $p = $productCategoryRepository->findOneBy(['id' => $rp['parentId']]);
+            if ($p) {
+                $pc->setParent($p);
+            }
+        }
+
+        $productCategoryRepository->save($pc);
+
+        return $this->json(['payload' => $this->serialize($pc)]);
+    }
 }
