f36332245e9180dd6b91f36074da160251771faf
Alex Vasechkin <vasechkin.alex.v@gmail.com>
2022-11-17 10:14:40 +0300

    Маршрут для отрисовки редактора категорий

---
+++ src/Controller/ControlPanel/ProductCategoryEditorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\ControlPanel;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductCategoryEditorController extends AbstractController
+{
+    /**
+     * @Route("/control-panel/product-category", methods={"GET"})
+     */
+    public function main()
+    {
+        return $this->render(':cp:product-category-editor.html.twig', []);
+    }
+}
