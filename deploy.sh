68e70a055055da0b81459d112d4f3b9db0eb8db2
Александр <administrator@MacBook-Pro-Aleksandr.local>
2023-07-26 19:28:44 +0300

    билд через npm

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,4 @@ php ./composer.phar config --no-plugins allow-plugins.php-http/discovery true &&
 php ./composer.phar install &&
 php bin/console doctrine:migrations:migrate
 
-apt update
-apt install -y nodejs
-
 cd /var/www/html && npm run build
