7ce25d3511d9b6e0d9d3acfd35cfc62fc51f4d80
Александр <administrator@MacBook-Pro-Aleksandr.local>
2023-07-26 19:33:50 +0300

    install npm

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,9 @@ php ./composer.phar config --no-plugins allow-plugins.php-http/discovery true &&
 php ./composer.phar install &&
 php bin/console doctrine:migrations:migrate
 
-cd /var/www/html && npm install && npm run build
+cd /var/www/html
+apt update
+apt install -y npm
+
+npm install
+npm run build
